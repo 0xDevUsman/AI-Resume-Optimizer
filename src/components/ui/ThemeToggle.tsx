1bc849a36e63b5ac574a24d9ac9b48c728ac3f2e
0xDevUsman <ua1226925@gmail.com>
2025-06-06 15:59:52 +0530

    initialize project

---
+++ src/components/ui/ThemeToggle.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Switch } from "@/components/ui/switch"; // from ShadCN
+import { useEffect, useState } from "react";
+import { LuSun } from "react-icons/lu";
+import { FaMoon } from "react-icons/fa6";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Avoid hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const isDark = theme === "dark";
+
+  return (
+    <div className="flex justify-between items-center gap-3 p-6">
+      <span className="text-lg text-muted-foregrouand">
+        {theme === "dark" ? <FaMoon /> : <LuSun />}
+      </span>
+      <Switch
+        checked={isDark}
+        onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
+        className="transition-ease duration-1000"
+      />
+    </div>
+  );
+}
